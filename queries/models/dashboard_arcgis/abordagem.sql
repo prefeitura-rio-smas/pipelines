f92705637f4842df56b49abfa488f53d4f4382e5
Luisa Martins <luisapilar.m@gmail.com>
2025-06-27 14:47:26 +0000

    feature: criação da tabela abordagem, estruturação dos arquivos na pasta staging.

---
+++ queries/models/dashboard_arcgis/abordagem.sql
@@ -0,0 +1,21 @@
+{{ config(materialized = 'table') }}
+
+SELECT
+  r.*,
+  e.cas,
+  e.e_mail,
+  c.nm,
+  c.mm,
+  c.cluster_id,
+  c.cluster_size,
+  c.is_duplicado  
+FROM 
+  {{ ref('abordagem_repeat') }} as r
+LEFT JOIN 
+  `rj-smas-dev.dashboard_arcgis_dev.abordagem_filtro_emails` as e
+ON
+  r.repeat_unidade_cas = e.cas
+LEFT JOIN  
+  {{ ref('abordagem_repeat_tratamento_duplicatas_cluster') }} as c
+ON
+  r.globalid = c.globalid
--- queries/models/dashboard_arcgis/staging/abordagem_repeat.sql
+++ queries/models/dashboard_arcgis/staging/abordagem_repeat.sql
@@ -1,3 +1,4 @@
+{{ config(materialized = 'ephemeral') }}
 
 SELECT
   objectid,
