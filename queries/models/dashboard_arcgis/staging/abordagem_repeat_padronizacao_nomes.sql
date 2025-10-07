b5e3a6caeadca694f4c844dc1fa8899d59224c8b
Victor Rodrigues <contactvictorcrespo@gmail.com>
2025-10-07 10:42:15 -0300

    fix: Recuperando a pasta dashboard_arcgis

---
+++ queries/models/dashboard_arcgis/staging/abordagem_repeat_padronizacao_nomes.sql
@@ -0,0 +1,51 @@
+{{ config(materialized='ephemeral') }}
+
+WITH base AS (
+  SELECT
+    globalid,
+    parentrowid,
+    repeat_nome_usuario,
+    repeat_nome_mae,
+    ano_num_data_abordagem
+  FROM {{ source('arcgis_raw', 'abordagem_repeat_raw') }}
+),
+
+tokens AS (
+  SELECT
+    *,
+
+    -- explode em palavras já normalizadas
+    SPLIT({{ clean_name('repeat_nome_usuario') }}, ' ')     AS arr_usuario,
+    SPLIT({{ clean_name('repeat_nome_mae') }},     ' ')     AS arr_mae
+  FROM base
+),
+
+filtered AS (
+  SELECT
+    *,
+
+    -- remove stop-words simples
+    ARRAY(
+      SELECT t
+      FROM UNNEST(arr_usuario) t
+      WHERE t NOT IN ('de','da','do','das','dos','e','a','o','os','as')
+    ) AS arr_usuario_ok,
+
+    ARRAY(
+      SELECT t
+      FROM UNNEST(arr_mae) t
+      WHERE t NOT IN ('de','da','do','das','dos','e','a','o','os','as')
+    ) AS arr_mae_ok
+  FROM tokens
+)
+
+SELECT
+  globalid,
+  parentrowid,
+  repeat_nome_usuario,
+  repeat_nome_mae,
+  ano_num_data_abordagem,
+
+  ARRAY_TO_STRING(arr_usuario_ok, ' ')  AS nome_usuario_norm,
+  ARRAY_TO_STRING(arr_mae_ok,     ' ')  AS nome_mae_norm
+FROM filtered
