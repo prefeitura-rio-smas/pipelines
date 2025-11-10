cd3b4ede3dae12a01714c1bb3d8a94433d68a6e3
Victor Rodrigues <contactvictorcrespo@gmail.com>
2025-11-10 10:29:47 -0300

    feat: criação do model contas_associadas

---
+++ queries/models/dashboard_acolherio/contas_associadas.sql
@@ -0,0 +1,22 @@
+{{ config(materialized = 'table') }}
+
+WITH contas_associadas AS (
+    SELECT
+    us.dscus AS unidade,
+    contas.seqlogin,
+    contas.nompess AS OPERADOR,
+    cbo.dsccbo AS PROFISSIONAL,
+    prof.cpfprof AS CPF,
+    contas.dscemail AS EMAIL,
+    prof.dsctel AS TELEFONE,
+    {{ map_coluna_perfil_acesso('contas.indnivel')}},
+    {{ map_coluna_status_conta('contas.indstatuser')}}
+    FROM {{ source('brutos_acolherio_staging', 'gh_contas')}} contas
+    LEFT JOIN {{ source('brutos_acolherio_staging', 'gh_prof')}} prof ON prof.seqlogin = contas.seqlogin
+    LEFT JOIN {{ source('brutos_acolherio_staging', 'gh_contas_us')}} contas_us ON contas_us.seqlogin = prof.seqlogin
+    LEFT JOIN {{ source('brutos_acolherio_staging', 'gh_us')}} us ON contas_us.sequs = us.sequs
+    LEFT JOIN {{ source('brutos_acolherio_staging', 'gh_profocup')}} ocup ON ocup.seqprof = prof.seqprof
+    LEFT JOIN {{ source('brutos_acolherio_staging', 'gh_cbo')}} cbo ON cbo.codcbo = ocup.codcbo
+)
+
+SELECT * FROM contas_associadas
