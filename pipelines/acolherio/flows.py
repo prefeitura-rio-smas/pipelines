dfea57f5699d4760481a2aaba21d493463206682
Victor Rodrigues <contactvictorcrespo@gmail.com>
2025-12-30 15:44:25 -0300

    fix: Alteração no nome para pegar no cicd

--- pipelines/acolherio/flows.py
+++ pipelines/acolherio/flows.py
@@ -3,8 +3,8 @@ from pipelines.tasks import run_dbt_models
 
 
 @flow(name='dbt_acolherio_rma')
-def acolherio_pipeline():
+def acolherio_flow() -> None:
     run_dbt_models('stg_filtro_evolucao')
 
 if __name__ == "__main__":
-    acolherio_pipeline()
+    acolherio_flow()
