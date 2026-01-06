00220ec1f7a0329ec8a8d803b189ee9edbbaf6db
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-06 11:30:31 -0300

    chore: teste pipeline auth

--- pipelines/acolherio/flows.py
+++ pipelines/acolherio/flows.py
@@ -1,10 +1,14 @@
 from prefect import flow
 from pipelines.tasks import run_dbt_models
+from constants import configure_gcp_credentials
 
 
 @flow(name='dbt_acolherio_rma')
 def acolherio_flow() -> None:
+    configure_gcp_credentials()
     run_dbt_models('stg_filtro_evolucao')
+    
+
 
 if __name__ == "__main__":
     acolherio_flow()
