89ac2b19d332a62df7b0eebb3273330eea717abc
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-12 16:41:23 -0300

    refactor: criação do models responsável por retornar os operadores com suas unidades associadas

---
+++ queries/models/rma/cras/staging/stg_contas_por_unidades.sql
@@ -0,0 +1,16 @@
+-- Tabela retorna todas as contas do sistema acolherio e suas respectivas unidades;
+-- Retorna contas que não possuem unidades;
+-- Retira contas de testes, admin e suporte;
+
+with contas_por_unidade as (
+    select
+        a.login_operador,
+        a.operador,
+        a.seqlogin,
+        b.unidade
+    from {{ ref ('base_contas') }} a
+    left join  {{ ref ('base_contas_unidades') }} b on a.seqlogin = b.seqlogin
+)
+
+select * from contas_por_unidade
+where not regexp_contains(operador, r'(?i)teste|admin|suporte')
