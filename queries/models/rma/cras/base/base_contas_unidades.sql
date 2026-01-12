ce5591a26282ff2656ad0c3dfcc0e9a2b28708ea
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-12 16:43:35 -0300

    refactor: filtrando apenas pela a unidade mais recente que o operador logou no sistema

--- queries/models/rma/cras/base/base_contas_unidades.sql
+++ queries/models/rma/cras/base/base_contas_unidades.sql
@@ -1,5 +1,6 @@
 -- Tabela responsável por retornar os operadores e suas respectivas unidades
 -- Existem contas com mais de uma unidade.
+-- Contas testes, admin e suporte não foram retiradas
 
 with contas_mais_de_uma_unidade as (
 select 
@@ -13,3 +14,4 @@ order by datacesso asc
 )
 
 select * from contas_mais_de_uma_unidade 
+where n_number = 1
