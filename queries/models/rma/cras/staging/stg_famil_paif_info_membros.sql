d467879d1687198222210162af06ea45bf318fd7
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-12 16:40:12 -0300

    refactor: criação da staging responsável por retornar todas as informações de cada membro da família em acompanhamento PAIF

---
+++ queries/models/rma/cras/staging/stg_famil_paif_info_membros.sql
@@ -0,0 +1,44 @@
+-- Tabela retorna todos os membros de cada família que está em acompanhamento PAIF e a informação indivídual de cada membro da família;
+-- Tabela não retorna nenhum usuário teste
+
+with informacao_membros as (
+    select 
+    b.seqfamil,
+    a.seqpac,
+    a.nome_usuario,
+    a.data_nascimento,
+    a.sexo,
+    a.racacor,
+    from {{ ref('base_cidadao_pac') }} a
+    inner join {{ ref('base_familia_membros') }} b on a.seqpac = b.seqpac
+),
+
+tratar_idade_membro as (
+    select
+        seqfamil,
+        seqpac,
+        nome_usuario,
+        data_nascimento,
+        extract(day from data_nascimento) as dia_nascimento,
+        extract(month from data_nascimento) as mes_nascimento,
+        date_diff(current_date(), data_nascimento, year) as idade,
+        sexo,
+        racacor
+    from informacao_membros
+)
+
+select
+    a.seqfamil,
+    a.seqpac,
+    a.nome_usuario,
+    a.data_nascimento,
+    a.dia_nascimento,
+    a.mes_nascimento,
+    a.idade,
+    a.sexo,
+    a.racacor,
+    b.data_cadastro_paif,
+    b.seqlogincad
+    from tratar_idade_membro a
+    inner join {{ ref('base_servassist') }} b on a.seqfamil = b.seqfamil
+
