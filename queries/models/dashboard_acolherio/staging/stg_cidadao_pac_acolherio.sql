c30c53f7d7085c431f73824c751cd4bce620f0b1
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-27 15:04:46 -0300

    feat: criação do model relacionado ao gh_cidadao_pac, assim como seus testes.

---
+++ queries/models/dashboard_acolherio/staging/stg_cidadao_pac_acolherio.sql
@@ -0,0 +1,24 @@
+-- Tabela responsável por retornar parte dos dados relacionados ao usuário
+-- Contém usuários testes
+-- Não contém usuários repetidos
+
+{{ config(materialized='table')}}
+
+with dados_cidadao_pac as (
+    select
+        seqpac,
+        dscnomepac as nome_usuario,
+        dscnomsoci as nome_social,
+        dscnmmae as filiacao_mae,
+        datnascim as data_nascimento,
+        nacional as nacionalidade,
+        condestr as condicao_estrangeira,
+        paisorigem as pais_origem,
+        racacor as raca, 
+        numcpfpac as cpf,
+        indsexo as sexo,
+        indgenero as genero
+    from  {{ source('source_dashboard_acolherio', 'gh_cidadao_pac') }}
+)
+
+select * from dados_cidadao_pac
