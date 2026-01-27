9a104ca040bf48e6a3f709b2bb411f6894446cdf
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-27 13:35:18 -0300

    feat: criação do model  e yml descritivo com testes pacientes_sm. além da criação da camada stg.

---
+++ queries/models/dashboard_acolherio/staging/stg_pacientes_sm.sql
@@ -0,0 +1,25 @@
+-- Tabela responsável por retornar parte dos dados relacionados ao usuário
+-- Contém usuários testes
+-- Não contém usuários repetidos
+
+{{ config(materialized='table')}}
+
+with dados_pacientes_sm as (
+    select
+        seqpacsm as seqpac,
+        indtrab as flag_trabalho,
+        nmprofi as profissao,
+        indfreqescol as flag_frequencia_escola,
+        indserie as serie_escola,
+        indescolari as escolaridade,
+        indrecbenef as flag_recebe_beneficio,
+        indqualbenef as tipo_beneficio,
+        indcuratela as flag_curatela, 
+        indtipcuratela as tipo_curatela,
+        indmoradi as flag_situacao_rua,
+        indpresdefi as flag_deficiencia,
+        indtpdefi as tipo_deficiencia
+    from  {{ source('source_dashboard_acolherio', 'gh_pacientes_sm') }}
+)
+
+select * from dados_pacientes_sm
