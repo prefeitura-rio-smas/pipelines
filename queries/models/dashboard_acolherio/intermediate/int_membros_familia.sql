56db1c2343f1c3d2a24011a6f9c6bbdecbd2de01
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-02-05 14:45:30 -0300

    refactor: refatoração do model pois existe um erro no sistema, onde os dados estão duplicando.

--- queries/models/dashboard_acolherio/intermediate/int_membros_familia.sql
+++ queries/models/dashboard_acolherio/intermediate/int_membros_familia.sql
@@ -1,10 +1,19 @@
 
-with filtrar_membros_familias_atuais as (
+with criar_id_usuario_repetidos_membros_familia as (
     select 
-        seqpac,
-        seqfamil
+    *, 
+    row_number() over(
+    partition  by seqpac order by seqpac desc
+    ) as n_usuario
     from {{ ref('stg_membros_familia_acolherio') }}
-    where  data_saida_membro_familia is null
+    where data_saida_membro_familia is null
+),
+
+usuarios_unicos_membro_familia as (
+    select
+        *
+    from criar_id_usuario_repetidos_membros_familia
+    where n_usuario = 1
 )
 
-select * from filtrar_membros_familias_atuais
+select * from usuarios_unicos_membro_familia
