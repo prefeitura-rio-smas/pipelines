86b557fe07fe42d40a191acec7ab926b7c87550f
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-03-02 16:12:32 -0300

    fix: erro do logger e caminho errado

--- pipelines/acolherio/flows.py
+++ pipelines/acolherio/flows.py
@@ -7,15 +7,16 @@ import os
 from pathlib import Path
 from prefect import get_run_logger
 
-logger = get_run_logger()
-
-logger.info(f"CWD: {os.getcwd()}")
-logger.info(f"FILES: {[p.name for p in Path('.').iterdir()]}")
 
 
 @flow(name="dbt_acolherio_rma")
 def acolherio_flow() -> None:
-    
+
+    logger = get_run_logger()
+
+    logger.info(f"CWD: {os.getcwd()}")
+    logger.info(f"FILES: {[p.name for p in Path('.').iterdir()]}")
+        
     dbt_target = os.getenv("MODE", "staging")
     trigger_dbt_cli_command(
         command=f"dbt run --select stg_filtro_evolucao --target {dbt_target}",
