6e2138c83be937564cce70cc2f8c9d22c1a8c9ff
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-03-04 12:37:50 -0300

    feat: inclusão de tipo de atendimento.

--- queries/models/dashboard_acolherio/mart/fact_atendimentos.sql
+++ queries/models/dashboard_acolherio/mart/fact_atendimentos.sql
@@ -6,11 +6,13 @@ with fato_atendimentos as (
         atend.seqatend_modulo,
         user.seqpac_sk,
         prof.seqprof_sk,
-        unid.sequs_sk
+        unid.sequs_sk,
+        tp_atend.seqtpatend_sk
     from {{ ref('int_atendimentos') }} atend 
     left join {{ ref('dim_usuarios') }} user on atend.seqpac = user.seqpac
     left join {{ ref('dim_profissionais') }} prof on atend.seqprof = prof.seqprof
     left join {{ ref('dim_unidades') }} unid on unid.sequs = atend.sequs
+    left join {{ ref('dim_tipo_atendimento') }} tp_atend on atend.seqtpatend = tp_atend.seqtpatend
 )
 
 select * from fato_atendimentos
