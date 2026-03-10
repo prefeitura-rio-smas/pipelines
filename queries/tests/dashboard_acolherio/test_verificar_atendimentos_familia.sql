989e3096dd77b1f4ed23da1a53413fb5cd56dc2a
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-03-10 10:55:15 -0300

    feat: Criação de testes para verificar se todos os atendimentos estão corretos.
    Criei dois testes para garantir que tanto os  atendimentos da camada staging do módulo usuário, como o do modulo família estão presentes na camada intermediária.

---
+++ queries/tests/dashboard_acolherio/test_verificar_atendimentos_familia.sql
@@ -0,0 +1,36 @@
+/*
+ Este teste verifica se todos os atendimentos do módulo família 
+ da camada staging estão na tabela int_atendimentos da camada 
+ intermediate.
+
+ Importante ressaltar que a int_atendimentos possui todos os atendimentos.
+ Ela possui atendimentos de todos os módulos (Usuário e Família)
+*/ 
+/*
+    Cte retorna todos os atendimentos módulo
+    de int_atendimentos, caso eles não existam na
+    stg_atendimentos_familias, o seqatend_modulo fica null. 
+*/
+with tabela_base as (
+select
+    a.seqatend_modulo,
+    a.modulo
+from {{ ref('int_atendimentos') }} a
+left join {{ ref('stg_atendimentos_familias') }} b
+on a.seqatend_modulo = b.seqatend_modulo
+)
+
+/*
+    Essa select retorna todos os casos em que o seqatend_modulo
+    é null e possui módulo família.
+
+    OBS: O teste unitário do dbt funciona retornando os casos
+    problemáticos, tendo em vista isso, eu formulei esse teste
+    para retorna os casos problemáticos. Se o teste não retornar
+    nada é por que a regra de negócio está correta.
+*/
+select
+    modulo
+from tabela_base
+where seqatend_modulo is null
+and modulo = 'f'
