bbcefd8fd638aec339ad39855ec60f4b970b1ec0
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-03-23 15:50:54 -0300

    chore: atualização cbo.

--- queries/models/dashboard_acolherio/intermediate/int_cbo.sql
+++ queries/models/dashboard_acolherio/intermediate/int_cbo.sql
@@ -1,11 +1,21 @@
 
-with cbo as (
+with cbo_joined as (
     select
-        {{ dbt_utils.generate_surrogate_key(['codcbo', 'seqprof']) }} as cbo_sk,
+        b.codcbo,
+        a.seqprof,
+        b.profissional
+    from {{ ref('stg_gh_prof_ocup') }} a
+    left join {{ ref('stg_cbo') }} b
+        on a.codcbo = b.codcbo
+),
+
+cbo as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['seqprof']) }} as seqprof_sk,
         codcbo,
         seqprof,
-        data_cadastro_cbo
-    from {{ ref('stg_cbo') }}
+        profissional,
+    from cbo_joined
 )
 
 select * from cbo
