996866c263ccd298f680bb82262ca877e6df7768
Enzo <17717607700@rio.rj.gov.br>
2026-08-04 12:09:47 -0300

    ajuste de macro

--- queries/macros/acolherio/extrair_ultima_atualizacao.sql
+++ queries/macros/acolherio/extrair_ultima_atualizacao.sql
@@ -1,3 +1,3 @@
 {% macro extrair_ultima_atualizacao(tabela) %}
-    select max(data_extracao_origem) as ultima_atualizacao from {{ ref(tabela) }}
+    (select max(data_extracao_origem) from {{ ref(tabela) }})
 {% endmacro %}
