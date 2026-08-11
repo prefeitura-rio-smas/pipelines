134a196a600c47e47a4a2f4536d1296d1c25b34f
enzostana <enzostana@proton.me>
2026-08-11 22:34:09 +0200

    refactor(cadunico): simplifica macro da última partição e aplica tag weekly nos modelos raw

--- queries/macros/cadunico/filtro_particao_cadunico.sql
+++ queries/macros/cadunico/filtro_particao_cadunico.sql
@@ -1,9 +1,9 @@
 {% macro obter_particao_cadunico() %}
-    {%- set m = run_query("select parse_date('%Y%m', max(partition_id)) from `rj-smas.protecao_social_cadunico.documento_membro$__PARTITIONS_SUMMARY__`").columns[0].values()[0] if execute else none -%}
+    {%- set m = run_query("select max(data_particao) from `rj-smas.protecao_social_cadunico.prefeitura`").columns[0].values()[0] if execute else none -%}
     {%- do return(m) -%}
 {% endmacro %}
 
 {% macro filtro_particao_cadunico() %}
     {%- set p = obter_particao_cadunico() -%}
-    {%- if p -%}data_particao >= date('{{ p }}') and data_particao < date_add(date('{{ p }}'), interval 1 month){%- endif -%}
+    {%- if p -%}data_particao = date('{{ p }}'){%- endif -%}
 {% endmacro %}
