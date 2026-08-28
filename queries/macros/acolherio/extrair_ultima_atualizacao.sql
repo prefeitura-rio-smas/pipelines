ef6fe960f4ca1beba174f9b4370b377ae3e78d61
enzostana <enzostana@proton.me>
2026-08-28 16:43:28 +0200

    fix(macros): usa datetime() na conversao UTC->SP (evita erro de parser do BigQuery)
    'AT TIME ZONE' como postfix apos funcao dentro de outra chamada e
    rejeitado pelo parser ('Expected ) but got keyword AT'). datetime(
    timestamp(col), 'America/Sao_Paulo') faz a mesma conversao para
    DATETIME em horario local e e sintaxe de funcao pura.

--- queries/macros/acolherio/extrair_ultima_atualizacao.sql
+++ queries/macros/acolherio/extrair_ultima_atualizacao.sql
@@ -1,3 +1,3 @@
 {% macro extrair_ultima_atualizacao(tabela) %}
-    (select max(timestamp(data_extracao_origem) at time zone 'America/Sao_Paulo') from {{ ref(tabela) }})
+    (select max(datetime(timestamp(data_extracao_origem), 'America/Sao_Paulo')) from {{ ref(tabela) }})
 {% endmacro %}
