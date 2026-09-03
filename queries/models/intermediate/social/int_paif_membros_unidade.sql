5ee06e78ad4a49618f6ca020c897e982de753f2f
enzostana <enzostana@proton.me>
2026-09-03 16:51:45 +0200

    fix(rma_cras): fallback de unidade via operador de cadastro no int_paif_membros_unidade

--- queries/models/intermediate/social/int_paif_membros_unidade.sql
+++ queries/models/intermediate/social/int_paif_membros_unidade.sql
@@ -4,11 +4,20 @@ with paif as (
     select
         id_familia,
         id_unidade,
+        id_login_cadastro,
         data_cadastro as data_cadastro_paif
     from {{ ref('raw_familias_servicos_assistenciais') }}
     where
         id_servico_assistencial = 1
         and data_cancelamento is null
+),
+
+unidade_por_login as (
+    select
+        id_login,
+        array_agg(id_unidade order by id_unidade limit 1)[offset(0)] as id_unidade
+    from {{ ref('raw_operadores_unidades') }}
+    group by id_login
 ),
 
 membros as (
@@ -44,18 +53,19 @@ vulnerabilidades_familia as (
 
 final as (
     select
-        p.id_unidade,
         p.id_familia,
         p.data_cadastro_paif,
         m.id_usuario,
         u.data_nascimento,
         u.beneficio,
         u.violacoes,
-        vf.vulnerabilidades
+        vf.vulnerabilidades,
+        coalesce(p.id_unidade, ul.id_unidade) as id_unidade
     from paif as p
     inner join membros as m on p.id_familia = m.id_familia
     inner join usuarios as u on m.id_usuario = u.id_usuario
     left join vulnerabilidades_familia as vf on p.id_familia = vf.id_familia
+    left join unidade_por_login as ul on p.id_login_cadastro = ul.id_login
 )
 
 select * from final
